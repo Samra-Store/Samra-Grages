3b749f75d223a719acf8785eb37baa2d2ecf2a62
Samra Store <moabosamra5@gmail.com>
2026-05-31 21:13:09 +0300

    Add quad bike configurations to SS-QuadBikes.lua
    Added configurations for various quad bike models including their names, prices, and image links.

---
+++ SS-QuadBikes[Debaged].lua
@@ -0,0 +1,8 @@
+  ["SS-QuadBikes"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+  ["SS-13OutlanderMaxXT-D"] = {"Can-Am OutlanderMaxXT 2013", 105000, "<img class='CarImg' src='https://i.ibb.co/RGf0k5J1/1.png' width='100' height='50' >"},
+  ["SS-14RaptorR1M-D"] = {"Yamaha R1M Raptor 2014", 75000, "<img class='CarImg' src='https://i.ibb.co/hxyFHdV8/2.png' width='100' height='50' >"},
+  ["SS-14Renegade-D"] = {"Can-Am Renegade 2014", 135000, "<img class='CarImg' src='https://i.ibb.co/9BmBXwQ/3.png' width='100' height='50' >"},
+  ["SS-OutlanderMax2026-D"] = {"Can-Am OutlanderMax 2026", 120000, "<img class='CarImg' src='https://i.ibb.co/1GtYkZdg/4.png' width='100' height='50' >"},
+},
