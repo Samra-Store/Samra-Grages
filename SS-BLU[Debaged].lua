a8834aafb01245ae9956f92828d4f5de3d60caf5
Samra Store <moabosamra5@gmail.com>
2026-05-31 21:12:17 +0300

    Add BLU edition vehicles to SS-BLU configuration
    Added various BLU edition vehicles with images and prices.

---
+++ SS-BLU[Debaged].lua
@@ -0,0 +1,15 @@
+  ["SS-BLU"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+  ["SS-BLU1-D"] = {"BLU Classic Taste", 150000, "<img class='CarImg' src='https://i.ibb.co/nNQRvxYq/1.png' >"},
+  ["SS-BLU2-D"] = {"BLU Tuti Fruti Taste", 150000, "<img class='CarImg' src='https://i.ibb.co/cXx9pcCD/2.png' width='100' height='50' >"},
+  ["SS-BLU3-D"] = {"BLU Mojito Taste", 150000, "<img class='CarImg' src='https://i.ibb.co/2BcKsW9/3.png' width='100' height='50' >"},
+  ["SS-BLU4-D"] = {"BLU Marshmello Taste", 150000, "<img class='CarImg' src='https://i.ibb.co/Tq7wFPGx/4.png' width='100' height='50' >"},
+  ["SS-BLU5-D"] = {"BLU Sugar Free Taste", 150000, "<img class='CarImg' src='https://i.ibb.co/hJhm0PVd/5.png' width='100' height='50' >"},
+  ["SS-BLU6-D"] = {"BLU Day Taste", 150000, "<img class='CarImg' src='https://i.ibb.co/nqZGMCsP/6.png' width='100' height='50' >"},
+  ["SS-BLU7-D"] = {"Mitsubishi L200 BLUE Edition", 200000, "<img class='CarImg' src='https://i.ibb.co/fGZrc44Y/image2.png' width='100' height='50' >"},
+  ["SS-BLU8-D"] = {"Trailer BLU Edition 1", 200000, "<img class='CarImg' src='https://i.ibb.co/vtCBwW6/image3.png' width='100' height='50' >"},
+  ["SS-BLU9-D"] = {"Trailer BLU Edition 2", 200000, "<img class='CarImg' src='https://i.ibb.co/TMPG5fBK/4.png' width='100' height='50' >"},
+  ["SS-BLU10-D"] = {"Mercedes Sprinter 2022 BLU Edition", 200000, "<img class='CarImg' src='https://i.ibb.co/wNv5xTj9/image.png' width='100' height='50' >"},      
+
+},
