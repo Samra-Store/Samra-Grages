43c90d518249a45b81ddaa644c03798103d36c4d
Samra Store <moabosamra5@gmail.com>
2026-06-13 12:45:42 +0300

    Add vehicle configurations for Palestine NSF cars

---
+++ SS-NSF-D.lua
@@ -0,0 +1,9 @@
+  ["SS-NSF"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+  ["SS-22SilveradoNSF-D"] = {"Plaestine NSF 1", 0, "<img class='CarImg' src='https://i.ibb.co/j94yR1D6/2022.png' width='100' height='50' >"},
+  ["SS-14SilveradoNSFS-D"] = {"Plaestine NSF 2", 0, "<img class='CarImg' src='https://i.ibb.co/gbqJRX7L/2014.png' width='100' height='50' >"},
+  ["SS-18ExpressNSF-D"] = {"Plaestine NSF 3", 0, "<img class='CarImg' src='https://i.ibb.co/ZzXJHsXn/2018.png' width='100' height='50' >"},
+  ["SS-10F150NSF-D"] = {"Plaestine NSF 4", 0, "<img class='CarImg' src='https://i.ibb.co/vFHCfLb/FORD.png' width='100' height='50' >"},
+  ["SS-25TransporterNSF-D"] = {"Plaestine NSF 5", 0, "<img class='CarImg' src='https://i.ibb.co/yBGxdYPr/2025.png' width='100' height='50' >"},
+},
