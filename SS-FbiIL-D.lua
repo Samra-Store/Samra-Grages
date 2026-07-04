030280ebd9e08be89e06b8685662bbf49c9d2249
Samra Store <moabosamra5@gmail.com>
2026-07-04 13:28:48 +0300

    Fix image URLs for FBI vehicle entries
    Updated image URLs for FBI vehicles in SS-FbiIL-D.lua.

---
+++ SS-FbiIL-D.lua
@@ -0,0 +1,15 @@
+  ["SS-FbiIL-D"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77,permissions={"fbi.garage"}},  --<<<<-- برمشن الكراج 
+
+  ["SS-FbiIL1-D"] = {"IL - FBI 1", 0, "<img class='CarImg' src='https://i.ibb.co/5hRLLBSK/1.png' width='100' height='50' >"},
+  ["SS-FbiIL2-D"] = {"IL - FBI 2", 0, "<img class='CarImg' src='https://i.ibb.co/b59FdzxT/2.png' width='100' height='50' >"},
+  ["SS-FbiIL3-D"] = {"IL - FBI 3", 0, "<img class='CarImg' src='https://i.ibb.co/7dpVcjZk/3.png' width='100' height='50' >"},
+  ["SS-FbiIL4-D"] = {"IL - FBI 4", 0, "<img class='CarImg' src='https://i.ibb.co/TqKGVPtp/4.png' width='100' height='50' >"},
+  ["SS-FbiIL5-D"] = {"IL - FBI 5", 0, "<img class='CarImg' src='https://i.ibb.co/99vGdxxh/5.png' width='100' height='50' >"},
+  ["SS-FbiIL6-D"] = {"IL - FBI 6", 0, "<img class='CarImg' src='https://i.ibb.co/H06GyM8/6.png' width='100' height='50' >"},
+  ["SS-FbiIL7-D"] = {"IL - FBI 7", 0, "<img class='CarImg' src='https://i.ibb.co/67RkYw9X/7.png' width='100' height='50' >"},
+  ["SS-FbiIL8-D"] = {"IL - FBI 8", 0, "<img class='CarImg' src='https://i.ibb.co/prkZngsZ/8.png' width='100' height='50' >"},
+  ["SS-FbiIL9-D"] = {"IL - FBI 9", 0, "<img class='CarImg' src='https://i.ibb.co/Kpb0MJqP/9.png' width='100' height='50' >"},
+  ["SS-FbiIL10-D"] = {"IL - FBI 10", 0, "<img class='CarImg' src='https://i.ibb.co/QjqjkYsB/10.png' width='100' height='50' >"},      
+
+},
