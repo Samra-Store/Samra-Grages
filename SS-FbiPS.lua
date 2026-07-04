6dc98c0abfad52c457c840bfe4e05d7bf042b6c2
Samra Store <moabosamra5@gmail.com>
2026-07-04 13:10:30 +0300

    Add FBI vehicle configurations to SS-FbiPS.lua

--- SS-FbiPS.lua
+++ SS-FbiPS.lua
@@ -1,1 +1,15 @@
-s
+  ["SS-FbiPS"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+  ["SS-FbiPS1"] = {"PS - FBI 1", 0, "<img class='CarImg' src='https://i.ibb.co/4gNtm8sD/1.png' width='100' height='50' >"},
+  ["SS-FbiPS2"] = {"PS - FBI 2", 0, "<img class='CarImg' src='https://i.ibb.co/6JYWzXGr/2.png' width='100' height='50' >"},
+  ["SS-FbiPS3"] = {"PS - FBI 3", 0, "<img class='CarImg' src='https://i.ibb.co/sd8K6XTV/3.png' width='100' height='50' >"},
+  ["SS-FbiPS4"] = {"PS - FBI 4", 0, "<img class='CarImg' src='https://i.ibb.co/CKCY0hRB/4.png' width='100' height='50' >"},
+  ["SS-FbiPS5"] = {"PS - FBI 5", 0, "<img class='CarImg' src='https://i.ibb.co/SDmKC50L/5.png' width='100' height='50' >"},
+  ["SS-FbiPS6"] = {"PS - FBI 6", 0, "<img class='CarImg' src='https://i.ibb.co/h1VbNz6y/6.png' width='100' height='50' >"},
+  ["SS-FbiPS7"] = {"PS - FBI 7", 0, "<img class='CarImg' src='https://i.ibb.co/SXNMFxVv/7.png' width='100' height='50' >"},
+  ["SS-FbiPS8"] = {"PS - FBI 8", 0, "<img class='CarImg' src='https://i.ibb.co/gMfkjRcR/8.png' width='100' height='50' >"},
+  ["SS-FbiPS9"] = {"PS - FBI 9", 0, "<img class='CarImg' src='https://i.ibb.co/pBj51xHq/9.png' width='100' height='50' >"},
+  ["SS-FbiPS10"] = {"PS - FBI 10", 0, "<img class='CarImg' src='https://i.ibb.co/Fq4LrSzH/10.png' width='100' height='50' >"},      
+
+},
