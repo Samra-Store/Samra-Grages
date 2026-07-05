7d96193835a0394f649bdb3d0acbeed1aa8c159b
Samra Store <moabosamra5@gmail.com>
2026-07-05 15:07:31 +0300

    Fix key name for 2014 Silverado NSF entry

--- SS-NSF.lua
+++ SS-NSF.lua
@@ -2,7 +2,7 @@
     _config = {vtype="car",blipid=357,blipcolor=77},
 
   ["SS-22SilveradoNSF"] = {"Chevrolet Silverado 2022 NSF", 0, "<img class='CarImg' src='https://i.ibb.co/YTdW63ks/2022.png' width='100' height='50' >"},
-  ["SS-14SilveradoNSFS"] = {"Chevrolet Silverado 2014 NSF", 0, "<img class='CarImg' src='https://i.ibb.co/7Ndj8mtz/2014.png' width='100' height='50' >"},
+  ["SS-14SilveradoNSF"] = {"Chevrolet Silverado 2014 NSF", 0, "<img class='CarImg' src='https://i.ibb.co/7Ndj8mtz/2014.png' width='100' height='50' >"},
   ["SS-18ExpressNSF"] = {"Chevrolet Express 2018 NSF", 0, "<img class='CarImg' src='https://i.ibb.co/DfGbXF2B/2018.png' width='100' height='50' >"},
   ["SS-10F150NSF"] = {"Ford F-150 2010 NSF", 0, "<img class='CarImg' src='https://i.ibb.co/1YjhpPYR/FORD.png' width='100' height='50' >"},
   ["SS-25TransporterNSF"] = {"VW Transporter 2025 NSF", 0, "<img class='CarImg' src='https://i.ibb.co/QjJYWVwG/2025.png' width='100' height='50' >"},
