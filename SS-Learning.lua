8b83323a71e0b082681c6bf1fea2b11eed3b2fe5
Samra Store <moabosamra5@gmail.com>
2026-07-28 11:32:59 +0300

    Add SS-Learning configuration with training entries

---
+++ SS-Learning.lua
@@ -0,0 +1,8 @@
+  ["SS-Learning"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+  ["SS-Learning1"] = {"تدريب سواقة 1", 0, "<img class='CarImg' src='https://i.ibb.co/k2pwTmNt/1.png' width='100' height='50' >"},
+  ["SS-Learning2"] = {"تدريب سواقة 2", 0, "<img class='CarImg' src='https://i.ibb.co/ymsTBxLS/2.png' width='100' height='50' >"},
+  ["SS-Learning3"] = {"تدريب سواقة 3", 0, "<img class='CarImg' src='https://i.ibb.co/C3gsc5Q2/3.png' width='100' height='50' >"},
+  ["SS-Learning4"] = {"تدريب سواقة 4", 0, "<img class='CarImg' src='https://i.ibb.co/Z0nPZWL/4.png' width='100' height='50' >"},
+},
