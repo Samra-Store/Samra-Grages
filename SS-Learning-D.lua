0dbef1e866a88dfad3c71a6f23786beb6984e749
Samra Store <moabosamra5@gmail.com>
2026-07-28 11:32:25 +0300

    Add SS-Learning-D.lua with driving training data

---
+++ SS-Learning-D.lua
@@ -0,0 +1,8 @@
+  ["SS-Learning"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+  ["SS-Learning1-D"] = {"تدريب سواقة 1", 0, "<img class='CarImg' src='https://i.ibb.co/4Hp44Dr/1-D.png' width='100' height='50' >"},
+  ["SS-Learning2-D"] = {"تدريب سواقة 2", 0, "<img class='CarImg' src='https://i.ibb.co/ycT4bxRD/2-D.png' width='100' height='50' >"},
+  ["SS-Learning3-D"] = {"تدريب سواقة 3", 0, "<img class='CarImg' src='https://i.ibb.co/bRD1sXR7/3-D.png' width='100' height='50' >"},
+  ["SS-Learning4-D"] = {"تدريب سواقة 4", 0, "<img class='CarImg' src='https://i.ibb.co/99KJ3HDx/4-D.png' width='100' height='50' >"},
+},
