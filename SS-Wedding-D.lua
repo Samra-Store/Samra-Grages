ad75526e67d624bebc38a036538c86f7f855002f
Samra Store <moabosamra5@gmail.com>
2026-08-22 08:10:42 -0700

    Add SS-Wedding-D.lua with wedding car configurations

---
+++ SS-Wedding-D.lua
@@ -0,0 +1,14 @@
+  ["SS-Wedding"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+
+["SS-Wedding1-D"] = {"Wedding 1", 0, "<img class='CarImg' src='https://i.ibb.co/jkT5xNy2/1.png' width='100' height='50' >"},
+["SS-Wedding2-D"] = {"Wedding 2", 0, "<img class='CarImg' src='https://i.ibb.co/JZ6jWzp/2.png' width='100' height='50' >"},
+["SS-Wedding3-D"] = {"Wedding 3", 0, "<img class='CarImg' src='https://i.ibb.co/DhDQx56/3.png' width='100' height='50' >"},
+["SS-Wedding4-D"] = {"Wedding 4", 0, "<img class='CarImg' src='https://i.ibb.co/TBMsFxJT/4.png' width='100' height='50' >"},
+["SS-Wedding5-D"] = {"Wedding 5", 0, "<img class='CarImg' src='https://i.ibb.co/MyG31Xx1/5.png' width='100' height='50' >"},
+["SS-Wedding6-D"] = {"Wedding 6", 0, "<img class='CarImg' src='https://i.ibb.co/MyfgL2dy/6.png' width='100' height='50' >"},
+["SS-Wedding7-D"] = {"Wedding 7", 0, "<img class='CarImg' src='https://i.ibb.co/LznY5sjg/image-2026-08-22-080956385.png' width='100' height='50' >"},
+["SS-Wedding8-D"] = {"Wedding 8", 0, "<img class='CarImg' src='https://i.ibb.co/jPMW05SX/8.png' width='100' height='50' >"},
+
+    },
