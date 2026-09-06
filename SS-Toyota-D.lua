d292e33a94010d1839e8dc767ee3119d24d24651
Samra Store <moabosamra5@gmail.com>
2026-09-06 11:06:51 -0700

    Create SS-Toyota-D.lua

---
+++ SS-Toyota-D.lua
@@ -0,0 +1,12 @@
+  ["SS-Toyota"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+
+["SS-20CorollaHatchback-D"] = {"Toyota Corolla Hatchback 2020", 130000, "<img class='CarImg' src='https://i.ibb.co/JjTWXzxj/1.png' width='360' height='240' >"},
+["SS-20CorollaWagon-D"] = {"Toyota Corolla Wagon 2020", 160000, "<img class='CarImg' src='https://i.ibb.co/tMdf2bXY/2.png' width='360' height='240' >"},
+["SS-21PradoAS-D"] = {"Toyota Prado Custom", 360000, "<img class='CarImg' src='https://i.ibb.co/3yXKL5DR/3.png' width='360' height='240' >"},
+["SS-22CamryGR-D"] = {"Toyota Camry GR 2022", 180000, "<img class='CarImg' src='https://i.ibb.co/hJPmvv5J/4.png' width='360' height='240' >"},
+["SS-23CorollaCross-D"] = {"Toyota Corolla Cross 2023", 230000, "<img class='CarImg' src='https://i.ibb.co/hFCGqh46/5.png' width='360' height='240' >"},
+["SS-23Proace-D"] = {"Toyota Proace 2023", 140000, "<img class='CarImg' src='https://i.ibb.co/7NyCzy1n/6.png' width='360' height='240' >"},
+
+    },
