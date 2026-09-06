d3fac59138d22a5dabe597c18d3df885a113edb7
Samra Store <moabosamra5@gmail.com>
2026-09-06 11:02:34 -0700

    Add Toyota car models with configurations

---
+++ SS-Toyota.lua
@@ -0,0 +1,12 @@
+  ["SS-Toyota"] = { 
+    _config = {vtype="car",blipid=357,blipcolor=77},
+
+
+["SS-20CorollaHatchback"] = {"Toyota Corolla Hatchback 2020", 130000, "<img class='CarImg' src='https://i.ibb.co/vCDf11mF/1.png' width='360' height='240' >"},
+["SS-20CorollaWagon"] = {"Toyota Corolla Wagon 2020", 160000, "<img class='CarImg' src='https://i.ibb.co/23PVgWXC/2.png' width='360' height='240' >"},
+["SS-21PradoAS"] = {"Toyota Prado Custom", 360000, "<img class='CarImg' src='https://i.ibb.co/mV5WqsgP/3.png' width='360' height='240' >"},
+["SS-22CamryGR"] = {"Toyota Camry GR 2022", 180000, "<img class='CarImg' src='https://i.ibb.co/vvs2nq30/4.png' width='360' height='240' >"},
+["SS-23CorollaCross"] = {"Toyota Corolla Cross 2023", 230000, "<img class='CarImg' src='https://i.ibb.co/ZzhHGs7R/5.png' width='360' height='240' >"},
+["SS-23Proace"] = {"Toyota Proace 2023", 140000, "<img class='CarImg' src='https://i.ibb.co/Zz8GcYRr/6.png' width='360' height='240' >"},
+
+    },
